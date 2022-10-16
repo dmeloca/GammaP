1d16d562cee4366be641e858258c97258d9ae93f
eguar <emendezc@unal.edu.co>
2022-10-16 00:40:06 -0500

    generador de trayectorias tipo I y II, con un ancho del plano definido

--- GP.py
+++ GP.py
@@ -1,44 +1,66 @@
 from typing import List
 from pprint import pprint
 
-def grap_generate( plane:List[list],limit: int ) -> List[list] :
+import plotly.express as px
+import pandas as pd
+import plotly.graph_objects as go
+
+
+def TG_I(root:int ,counter: int) -> list[int]:
     '''
-    Graph generated from trajectories I, II, III.
-    Takes into account the number of dates that go to each point
+    Genera las trayectorias de tipo uno
     '''
+    x = [0, 1, 2, 3, 4, 5] #width
+    y = list()
+    slopes = list()
 
-    row: int = len(plane) -1
-    column: int = 1    
-    slope: int = 0
-    #type I trajectory
-    while column <= limit:
-        if row < 0 or column > len(plane)-1: 
-            break 
+    if counter >0: #blank spaces
+         for i in range(0, counter): y.append(None); slopes.append(None)
+    #initiation
+    slopes.append(0)
+    y.append(root) 
 
-        plane[row][column] = 1
+    for slope in range(0, len(x)-len(y)): y.append(slope+y[slope+counter]); slopes.append(slope) #points in y
 
-        slope += 1
-        row -= slope
-        column += 1
+    if counter == 0: #graphing
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(
+        x=x,
+        y=y,
+        name = 'Type I', 
+        legendgroup='Type I',
+        marker=dict(color='red'),))
+        return y,fig
 
-    #type II trajectory 
-    row: int = len(plane) -1
-    column: int = 1    
-    slope: int = 0
-    while column <= limit:
-        if row < 0 or column > len(plane)-1: 
-            break 
+    #fig.show()
 
-        plane[row][column] = 1
+    return y
 
-        slope += 1
-        row -= slope
-        column += 1
+def TG_II(TI:list[int], fig) :
+    '''
+    Genera las trayectorias de tipo dos
+    '''
+    x = [0, 1, 2, 3, 4, 5] #width
+
+    for i in range(1,len(TI)):
+        y = TG_I(TI[i],i )
+        c = True #legend
+        if i > 1: c = False 
+
+        fig.add_trace(go.Scatter(
+        x=x,
+        y=y,
+        name = 'Type II', 
+        legendgroup='Type II',
+        marker=dict(color='blue'),
+        showlegend= c,))
+        
+def TG_III():
+    pass
 
 
 
-
-    return plane
+    #fig.show()
 
 
 def m_g(c: int, r: int)-> List[list]:
@@ -51,86 +73,81 @@ def m_g(c: int, r: int)-> List[list]:
 
     return matrix
 
-import plotly.express as px
-import pandas as pd
+def testplots():
+    x = [0,1, 2, 3, 4, 5]
 
-import plotly.graph_objects as go
+    fig = go.Figure()
 
-x = [0,1, 2, 3, 4, 5]
-
-fig = go.Figure()
-
-fig.add_trace(go.Scatter(
-    x=x,
-    y=[0, 0, 1, 3 ],
-    name = 'Type I', 
-    legendgroup='Type I',
-    marker=dict(color='red'),
-))
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=[0, 0, 1, 3 ],
+        name = 'Type I', 
+        legendgroup='Type I',
+        marker=dict(color='red'),
+    ))
 
 
-fig.add_trace(go.Scatter(
-    x=x,
-    y=[None, 0, 0, 1, 3],
-    name = 'Type II', 
-    legendgroup='Type II',
-    marker=dict(color='blue'),
-))
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=[None, 0, 0, 1, 3],
+        name = 'Type II', 
+        legendgroup='Type II',
+        marker=dict(color='blue'),
+    ))
 
-fig.add_trace(go.Scatter(
-    x=x,
-    y=[None, None, 1, 1, 2],
-    name = 'Type II', 
-    legendgroup='Type II',
-    marker=dict(color='blue'),
-    showlegend=False,
-))
-
-
-fig.add_trace(go.Scatter(
-    x =x,
-    y =[None, None, None, 3, 3],
-    name = 'Type II', 
-    legendgroup='Type II',
-    marker=dict(color='blue'),
-    showlegend=False,
-))
-
-fig.add_trace(go.Scatter(
-    x =x,
-    y =[None, None, 0, 0],
-    name = 'Type III', 
-    legendgroup='Type III',
-    marker=dict(color='green'),
-))
-
-fig.add_trace(go.Scatter(
-    x =x,
-    y =[None, None, None, 1, 1, 2],
-    name = 'Type III', 
-    legendgroup='Type III',
-    marker=dict(color='green'),
-    showlegend=False,
-))
-
-fig.add_trace(go.Scatter(
-    x =x,
-    y =[None, None, None, None, 2, 2],
-    name = 'Type III', 
-    legendgroup='Type III',
-    marker=dict(color='green'),
-    showlegend=False,
-))
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=[None, None, 1, 1, 2],
+        name = 'Type II', 
+        legendgroup='Type II',
+        marker=dict(color='blue'),
+        showlegend=False,
+    ))
 
 
-fig.show()
+    fig.add_trace(go.Scatter(
+        x =x,
+        y =[None, None, None, 3, 3],
+        name = 'Type II', 
+        legendgroup='Type II',
+        marker=dict(color='blue'),
+        showlegend=False,
+    ))
 
+    fig.add_trace(go.Scatter(
+        x =x,
+        y =[None, None, 0, 0],
+        name = 'Type III', 
+        legendgroup='Type III',
+        marker=dict(color='green'),
+    ))
+
+    fig.add_trace(go.Scatter(
+        x =x,
+        y =[None, None, None, 1, 1, 2],
+        name = 'Type III', 
+        legendgroup='Type III',
+        marker=dict(color='green'),
+        showlegend=False,
+    ))
+
+    fig.add_trace(go.Scatter(
+        x =x,
+        y =[None, None, None, None, 2, 2],
+        name = 'Type III', 
+        legendgroup='Type III',
+        marker=dict(color='green'),
+        showlegend=False,
+    ))
+
+
+    fig.show()
+    
 
 
 if __name__ == "__main__":
-    pass
+    y, fig= TG_I(0,0)
+    
+    TG_II( y, fig)
 
-    #pprint(m_g(3, 6))
-    #print("")
-    #pprint(grap_generate( m_g(3,6),20))
 
