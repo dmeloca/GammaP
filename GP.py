7f6171ec7a50fa583fd01df7f336a279db688c71
Julio Vásquez <julcvasquezare@unal.edu.co>
2022-10-16 18:59:52 -0500

    Trayectorias y Lista de Clases

--- GP.py
+++ GP.py
@@ -1,153 +1,64 @@
-from typing import List
+from typing import List, Tuple
 from pprint import pprint
 
-import plotly.express as px
-import pandas as pd
 import plotly.graph_objects as go
 
+TYPE = ["Type I", "Type II", "Type III"]
+COLORS = ["red", "green", "blue"]
 
-def TG_I(root:int ,counter: int) -> list[int]:
+def Trajectory(points:list[list], t:int, fig:go.Figure, classes:list) -> list[list]:
     '''
-    Genera las trayectorias de tipo uno
+    Inserts trajectories of type t starting at every point in the list points into the figure fig
     '''
-    x = [0, 1, 2, 3, 4, 5] #width
-    y = list()
-    slopes = list()
-
-    if counter >0: #blank spaces
-         for i in range(0, counter): y.append(None); slopes.append(None)
-    #initiation
-    slopes.append(0)
-    y.append(root) 
-
-    for slope in range(0, len(x)-len(y)): y.append(slope+y[slope+counter]); slopes.append(slope) #points in y
-
-    if counter == 0: #graphing
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(
-        x=x,
-        y=y,
-        name = 'Type I', 
-        legendgroup='Type I',
-        marker=dict(color='red'),))
-        return y,fig
-
-    #fig.show()
-
-    return y
-
-def TG_II(TI:list[int], fig) :
-    '''
-    Genera las trayectorias de tipo dos
-    '''
-    x = [0, 1, 2, 3, 4, 5] #width
-
-    for i in range(1,len(TI)):
-        y = TG_I(TI[i],i )
-        c = True #legend
-        if i > 1: c = False 
-
-        fig.add_trace(go.Scatter(
-        x=x,
-        y=y,
-        name = 'Type II', 
-        legendgroup='Type II',
-        marker=dict(color='blue'),
-        showlegend= c,))
-        
-def TG_III():
-    pass
-
-
-
-    #fig.show()
-
-
-def m_g(c: int, r: int)-> List[list]:
-    '''
-    nxn matrix generator
-    '''
-    matrix = [[0 for col in range(c)] for row in range(r)]
-
-
-
-    return matrix
-
-def testplots():
-    x = [0,1, 2, 3, 4, 5]
-
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(
-        x=x,
-        y=[0, 0, 1, 3 ],
-        name = 'Type I', 
-        legendgroup='Type I',
-        marker=dict(color='red'),
-    ))
-
-
-    fig.add_trace(go.Scatter(
-        x=x,
-        y=[None, 0, 0, 1, 3],
-        name = 'Type II', 
-        legendgroup='Type II',
-        marker=dict(color='blue'),
-    ))
-
-    fig.add_trace(go.Scatter(
-        x=x,
-        y=[None, None, 1, 1, 2],
-        name = 'Type II', 
-        legendgroup='Type II',
-        marker=dict(color='blue'),
-        showlegend=False,
-    ))
-
-
-    fig.add_trace(go.Scatter(
-        x =x,
-        y =[None, None, None, 3, 3],
-        name = 'Type II', 
-        legendgroup='Type II',
-        marker=dict(color='blue'),
-        showlegend=False,
-    ))
-
-    fig.add_trace(go.Scatter(
-        x =x,
-        y =[None, None, 0, 0],
-        name = 'Type III', 
-        legendgroup='Type III',
-        marker=dict(color='green'),
-    ))
-
-    fig.add_trace(go.Scatter(
-        x =x,
-        y =[None, None, None, 1, 1, 2],
-        name = 'Type III', 
-        legendgroup='Type III',
-        marker=dict(color='green'),
-        showlegend=False,
-    ))
-
-    fig.add_trace(go.Scatter(
-        x =x,
-        y =[None, None, None, None, 2, 2],
-        name = 'Type III', 
-        legendgroup='Type III',
-        marker=dict(color='green'),
-        showlegend=False,
-    ))
-
-
-    fig.show()
+    newPoints = list() #List of points of type t
+    for p in points: #Every new trajectory of type t starts at point p
+        x = [p[0]]
+        y = [p[1]]
+        s = [p[2]] #max slope in the trajectories that cross the point (x, y)
+        slope = 0
+        '''
+        Checks if it is possible to draw a new line in the current trajectory
+        If the trajectory is of type 2 (III), the slope of every new line
+        must be lesser or equal than the slope of the previous lines
+        The coordinate y always must be lesser than 20
+        '''
+        while((t != 2 or slope <= s[-1]) and x[-1] < 9 and y[-1] + slope < 20):
+            x.append(x[-1] + 1)
+            y.append(y[-1] + slope)
+            s.append(max([slope, s[-1]]))
+            newPoints.append([x[-1], y[-1], s[-1]])
+            #Adds a unit into the correspondent class
+            if(x[-1] >= 0 and y[-1] >= 0):
+                classes[x[-1] + y[-1]] += 1
+            slope += 1
+        fig.add_trace(
+            go.Scatter(
+                x = x,
+                y = y,
+                name = TYPE[t], 
+                legendgroup = TYPE[t],
+                marker = dict(color=COLORS[t]),
+                text = s,
+                showlegend=False,
+            )
+        )
+    return newPoints
     
-
+def graphGenerator(x_0:int, y_0:int) -> go.Figure:
+    '''
+    Returns the graph of the trajectories of type I, II and III
+    and a list where the i-th position stores the number of trajectories
+    that cross the points of the class 'i' (points such as the sum of its
+    coordinates (x,y) is 'i'. x+y=i)
+    '''
+    fig = go.Figure()
+    classes = [0 for i in range(30)]
+    t1 = Trajectory([[x_0, y_0, 0]], 0, fig, classes) #Trajectory Type I
+    t2 = Trajectory(t1, 1, fig, classes) #Trajectories Type II
+    Trajectory(t2, 2, fig, classes) #Trajectories Type III
+    return fig, classes
 
 if __name__ == "__main__":
-    y, fig= TG_I(0,0)
-    
-    TG_II( y, fig)
-
-
+    fig, classes = graphGenerator(-5, -5)
+    fig.show()
+    print(classes)
